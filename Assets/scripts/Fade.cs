802407cb894c225224e2ff899acb25aace5e5f37
LuckyPdP <pdelpozodudda@gmail.com>
2026-02-26 23:21:21 +0100

    FadeIn

---
+++ Assets/scripts/Fade.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+
+    public Animator anim;
+    public GameObject FadeIn;
+
+
+    void Update()
+    {
+        FadeIn.SetActive(true);
+
+    }
+
+
+
+    public void Fadeout()
+    {
+        anim.Play("FadeOut");
+    }
+}
